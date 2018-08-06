9d5712dc777a939f77b46947786bc7be123e1f91
alipajand <ali.pajand@gmail.com>
2018-08-06 18:12:24 +0430

    :hammer: Rename project

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace BrokerClub
+namespace StarterKit
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BrokerClub
+namespace StarterKit
 {
     public class Startup
     {
